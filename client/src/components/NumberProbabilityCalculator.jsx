5c873d5834b8a3978064c989857fb7e997c02911
fruittypie <murmurr2013@gmail.com>
2024-03-01 22:59:17 -0600

    added component styling

--- client/src/components/NumberProbabilityCalculator.jsx
+++ client/src/components/NumberProbabilityCalculator.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import RollPictures from './RollPictures';
+import '../styles/NumberProbabilityCalculator.css'
 
 
 const NumberProbabilityCalculator = () => {
