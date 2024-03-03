03b0068769111270c3a29893f239b958a92008d5
fruittypie <murmurr2013@gmail.com>
2024-03-03 11:05:23 -0600

    set up Vite env
     set up Vite to use different environment files for development and production

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,7 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
+
+dotenv.config({
+  path: `./.env.${process.env.NODE_ENV || 'development'}`,
+});
 
 export default defineConfig({
   base: '/',
@@ -13,6 +18,9 @@ export default defineConfig({
       },
     }),
   ],
+  define: {
+    'process.env': process.env,
+  },
   build: {
     // Add esbuildOptions to handle JSX in react-drag-select library
     esbuildOptions: {
