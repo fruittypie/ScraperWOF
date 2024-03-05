634696fb879412b05400f39aa61a8d3b1ba7c0e9
fruittypie <murmurr2013@gmail.com>
2024-03-04 21:36:24 -0600

    added a new reducer to store

--- client/state/store.js
+++ client/state/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import numberReducer from './number/numberSlice';
 import totalNumberReducer from './number/totalNumSlice';
-
+import drawValuesReducer from './number/drawValuesSlice';
 
 const store = configureStore({
   reducer: {
     number: numberReducer,
     totalNum: totalNumberReducer,
+    drawValues: drawValuesReducer,
   },
 });
 
