079a6861223dd37509435425d2e3957eb8eafcd5
fruittypie <murmurr2013@gmail.com>
2024-03-10 19:15:50 -0500

    change the slice name

--- client/src/components/GapAnalyzer.jsx
+++ client/src/components/GapAnalyzer.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
-import { fetchDrawValues } from '../../state/number/drawValuesSlice';
+import { fetchAllNumbers } from '../../state/number/AllNumbersSlice';
 import '../styles/GapAnalyzer.css';
 
 async function findGapOccurrences(arr, num) {
@@ -27,14 +27,19 @@ async function findGapOccurrences(arr, num) {
 
 const GapAnalyzer = ({ selectedNumber }) => {
     const dispatch = useDispatch();
-    const drawValues = useSelector((state) => state.drawValues.drawValues);
+    const allNumbers = useSelector((state) => state.allNumbers.allNumbers);
     const [gapOccurrences, setGapOccurrences] = useState({}); 
+
+    const gapEntries = Object.entries(gapOccurrences);
+    const thirdLength = Math.ceil(gapEntries.length / 3);
+    const firstThird = gapEntries.slice(0, thirdLength);
+    const secondThird = gapEntries.slice(thirdLength, 2 * thirdLength);
+    const thirdThird = gapEntries.slice(2 * thirdLength);
 
     useEffect(() => {
         async function fetchDataAndProcess() {
-            if (!selectedNumber) return;
-
-            const values = drawValues.map(item => item.value); 
+            if (!selectedNumber || !allNumbers) return;
+            const values = allNumbers.map(item => item.value); 
             const gapOccurrences = await findGapOccurrences(values, selectedNumber);
 
             setGapOccurrences(gapOccurrences);
@@ -42,32 +47,79 @@ const GapAnalyzer = ({ selectedNumber }) => {
 
         fetchDataAndProcess();
 
-    }, [drawValues, selectedNumber]);
+    }, [allNumbers, selectedNumber]);
 
     useEffect(() => {
-        dispatch(fetchDrawValues());
+        dispatch(fetchAllNumbers());
     }, [dispatch]);
 
     return (
-        <div className="gap-container">
-            <div className="title-with-image">
-                <h2>Gaps for number {selectedNumber}</h2>
-                <div className="number-wrapper-color">
-                    <img
-                        src={`https://luckyrinawaybucket.s3.amazonaws.com/colors/Roll${selectedNumber}.png`} 
-                        alt={`Number ${selectedNumber}`}
-                    />
+        <div className="mask d-flex align-items-center h-100">
+            <div className="gap-container">
+                <div className="row justify-content-center">
+                    <div className="col">
+                        <div className="table-responsive">
+                            <table className="table table-dark table-bordered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">GAP</th>
+                                        <th scope="col">TIMES</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {firstThird.map(([gapLength, occurrence]) => (
+                                        <tr key={gapLength}>
+                                            <td>{gapLength}</td> 
+                                            <td>{occurrence}</td> 
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="table-responsive">
+                            <table className="table table-dark table-bordered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">GAP</th>
+                                        <th scope="col">TIMES</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {secondThird.map(([gapLength, occurrence]) => (
+                                        <tr key={gapLength}>
+                                            <td>{gapLength}</td> 
+                                            <td>{occurrence}</td> 
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="table-responsive">
+                            <table className="table table-dark table-bordered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">GAP</th>
+                                        <th scope="col">TIMES</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {thirdThird.map(([gapLength, occurrence]) => (
+                                        <tr key={gapLength}>
+                                            <td>{gapLength}</td> 
+                                            <td>{occurrence}</td> 
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div>                
-                <ul>
-                    {Object.entries(gapOccurrences).map(([gapLength, occurrence]) => (
-                        <li key={gapLength}>Gap: {gapLength}, Occurred: {occurrence} times</li>
-                    ))}
-                </ul>
             </div>
         </div>
     );
 };
-
 export default GapAnalyzer;
