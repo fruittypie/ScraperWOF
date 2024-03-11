d05128327ede2f38bd22bf7be4ce05d99dcd78df
fruittypie <murmurr2013@gmail.com>
2024-03-10 19:17:08 -0500

    add a new prop

--- client/src/components/RollPictures.jsx
+++ client/src/components/RollPictures.jsx
@@ -1,6 +1,6 @@
 import '../styles/RollPictures.css'
 
-const RollPictures = ({ onClick }) => {
+const RollPictures = ({ onClick, selectedNumber  }) => {
 
     return (
       
@@ -10,6 +10,7 @@ const RollPictures = ({ onClick }) => {
                     src={'https://luckyrinawaybucket.s3.amazonaws.com/colors/Roll1.png'} 
                     alt={'Number 1'}
                     onClick={() => onClick('1')}
+                    className={selectedNumber === '1' ? 'selected' : ''}
                 />
             </div>
             <div className="image-wrapper">
@@ -17,6 +18,7 @@ const RollPictures = ({ onClick }) => {
                     src={'https://luckyrinawaybucket.s3.amazonaws.com/colors/Roll3.png'} 
                     alt={'Number 3'}
                     onClick={() => onClick('3')}
+                    className={selectedNumber === '3' ? 'selected' : ''}
                 />
             </div>
 
@@ -25,6 +27,7 @@ const RollPictures = ({ onClick }) => {
                     src={'https://luckyrinawaybucket.s3.amazonaws.com/colors/Roll5.png'} 
                     alt={'Number 5'}
                     onClick={() => onClick('5')}
+                    className={selectedNumber === '5' ? 'selected' : ''}
                 />
             </div>
                 
@@ -33,6 +36,7 @@ const RollPictures = ({ onClick }) => {
                     src={'https://luckyrinawaybucket.s3.amazonaws.com/colors/Roll10.png'} 
                     alt={'Number 10'}
                     onClick={() => onClick('10')}
+                    className={selectedNumber === '10' ? 'selected' : ''}
                 />
             </div>
             <div className="image-wrapper">
@@ -40,6 +44,7 @@ const RollPictures = ({ onClick }) => {
                     src={'https://luckyrinawaybucket.s3.amazonaws.com/colors/Roll20.png'} 
                     alt={'Number 20'}
                     onClick={() => onClick('20')}
+                    className={selectedNumber === '20' ? 'selected' : ''}
                 />
             </div>
             
