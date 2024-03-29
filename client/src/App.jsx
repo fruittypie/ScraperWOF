ee828f3e27a3e31cd6a33618902e870d09207cdc
fruittypie <136939692+fruittypie@users.noreply.github.com>
2024-03-28 21:02:00 -0500

    make gap a default tab

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,39 +3,37 @@ import React, { useState } from 'react';
 import NumberList from './components/NumberList.jsx';
 import CurrentNumber from './components/CurrentNumber.jsx';
 import CurrentGap from './components/CurrentGap.jsx';
+import TotalSum from './components/TotalSum.jsx';
 import NumberProbabilityCalculator from './components/NumberProbabilityCalculator.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [latestNumber, setLatestNumber] = useState(null);
   const [selectedNumbers, setSelectedNumbers] = useState([]); 
-  const [activeTab, setActiveTab] = useState('calculator'); // State to track active tab
+  const [activeTab, setActiveTab] = useState('gap');
 
   return (
     <div className="app-container">
       <div className="row">
       <div className="col-md-5" style={{ backgroundColor: '#343434' }}>
-      <nav>
-        <div className="nav nav-tabs" id="nav-tab" role="tablist">
-          <button
-            className={`nav-link ${activeTab === 'calculator' ? 'active' : ''}`}
-            onClick={() => setActiveTab('calculator')}
-          >
-            Gap
-          </button>
-          <button
-            className={`nav-link ${activeTab === 'gap' ? 'active' : ''}`}
-            onClick={() => setActiveTab('gap')}
-          >
-            Calculator
-          </button>
-        </div>
-      </nav>
+        <nav>
+          <div className="nav nav-tabs d-flex justify-content-between align-items-center" id="nav-tab" role="tablist">
+            <div className="d-flex">
+              <button className={`nav-link ${activeTab === 'gap' ? 'active' : ''}`} onClick={() => setActiveTab('gap')}>
+                Gap
+              </button>
+              <button className={`nav-link ${activeTab === 'calculator' ? 'active' : ''}`} onClick={() => setActiveTab('calculator')}>
+                Calculator
+              </button>
+            </div>
+            <TotalSum />
+          </div>
+        </nav>
       <div className="tab-content" id="nav-tabContent">
-        <div className={`tab-pane fade ${activeTab === 'calculator' ? 'show active' : ''}`}>
+        <div className={`tab-pane fade ${activeTab === 'gap' ? 'show active' : ''}`}>
           <CurrentGap />
         </div>
-        <div className={`tab-pane fade ${activeTab === 'gap' ? 'show active' : ''}`}>
+        <div className={`tab-pane fade ${activeTab === 'calculator' ? 'show active' : ''}`}>
           <NumberProbabilityCalculator />
         </div>
       </div>
