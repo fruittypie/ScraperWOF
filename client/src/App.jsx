b24576d18856a8086342b4b38c5c9285720eff91
fruittypie <136939692+fruittypie@users.noreply.github.com>
2024-03-30 10:52:20 -0500

    change the input format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,35 +15,34 @@ function App() {
   return (
     <div className="app-container">
       <div className="row">
-      <div className="col-md-5" style={{ backgroundColor: '#343434' }}>
-        <nav>
-          <div className="nav nav-tabs d-flex justify-content-between align-items-center" id="nav-tab" role="tablist">
-            <div className="d-flex">
-              <button className={`nav-link ${activeTab === 'gap' ? 'active' : ''}`} onClick={() => setActiveTab('gap')}>
-                Gap
-              </button>
-              <button className={`nav-link ${activeTab === 'calculator' ? 'active' : ''}`} onClick={() => setActiveTab('calculator')}>
-                Calculator
-              </button>
+        <div className="col-md-5" style={{ backgroundColor: '#343434' }}>
+          <nav>
+            <div className="nav nav-tabs d-flex justify-content-between align-items-center" id="nav-tab" role="tablist">
+              <div className="d-flex">
+                <button className={`nav-link ${activeTab === 'gap' ? 'active' : ''}`} onClick={() => setActiveTab('gap')}>
+                  Gap
+                </button>
+                <button className={`nav-link ${activeTab === 'calculator' ? 'active' : ''}`} onClick={() => setActiveTab('calculator')}>
+                  Calculator
+                </button>
+              </div>
+              <TotalSum />
             </div>
-            <TotalSum />
+          </nav>
+          <div className="tab-content" id="nav-tabContent">
+            <div className={`tab-pane fade ${activeTab === 'gap' ? 'show active' : ''}`}>
+              <CurrentGap />
+            </div>
+            <div className={`tab-pane fade ${activeTab === 'calculator' ? 'show active' : ''}`}>
+              <NumberProbabilityCalculator />
+            </div>
           </div>
-        </nav>
-      <div className="tab-content" id="nav-tabContent">
-        <div className={`tab-pane fade ${activeTab === 'gap' ? 'show active' : ''}`}>
-          <CurrentGap />
         </div>
-        <div className={`tab-pane fade ${activeTab === 'calculator' ? 'show active' : ''}`}>
-          <NumberProbabilityCalculator />
+        <div className="col-md-7" style={{ backgroundColor: '#343434' }}>
+          <CurrentNumber latestNumber={latestNumber} />
+          <NumberList setLatestNumber={setLatestNumber} selectedNumbers={selectedNumbers} setSelectedNumbers={setSelectedNumbers} />
         </div>
       </div>
-      </div>
-      <div className="col-md-7" style={{ backgroundColor: '#343434' }}>
-        <CurrentNumber latestNumber={latestNumber} />
-        <NumberList setLatestNumber={setLatestNumber} selectedNumbers={selectedNumbers} setSelectedNumbers={setSelectedNumbers} />
-      </div>
-      </div>
-
     </div>
   );
 }
